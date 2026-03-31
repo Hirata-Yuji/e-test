255c05f420a217519e5bb2aa39d17b86c6b93458
Hirata-Yuji <hirata@enjin-group.com>
2026-03-31 19:15:53 +0900

    Delete en-journey directory

--- en-journey/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
-  basePath: '/e-test',
-  assetPrefix: '/e-test/',
-}
-module.exports = nextConfig
